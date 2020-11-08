7d3a67331e1f957bf485750aa8808e838b904227
Naomi lee <lyc0503@gmail.com>
2020-11-09 01:08:02 +0800

    Initial Commit

---
+++ Sims with me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sims with me
+//
+//  Created by APPLE on 2020/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
